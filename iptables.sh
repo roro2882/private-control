ac451fc1f536f6394db0d1ee3fe406cd009639f5
John Doe <johndoe@example.com>
2022-07-06 10:24:36 +0200

    rm some files

--- iptables.sh
+++
@@ -1,6 +0,0 @@
-ip6tables --policy INPUT DROP;
-ip6tables --policy OUTPUT DROP;
-ip6tables --policy FORWARD DROP;
-iptables --policy INPUT DROP;
-iptables --policy OUTPUT DROP;
-iptables --policy FORWARD DROP;
--- rmiptables.sh
+++
@@ -1,6 +0,0 @@
-ip6tables --policy INPUT ACCEPT;
-ip6tables --policy OUTPUT ACCEPT;
-ip6tables --policy FORWARD ACCEPT;
-iptables --policy INPUT ACCEPT;
-iptables --policy OUTPUT ACCEPT;
-iptables --policy FORWARD ACCEPT;
